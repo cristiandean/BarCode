db5ffc3385418dcb534defa94979e7236ccbd353
Cristian Dean <cristiandeanabreu@gmail.com>
2017-05-28 23:59:20 -0300

    Resolution of problems

--- src/Controller/Component/BarCodeComponent.php
+++ src/Controller/Component/BarCodeComponent.php
@@ -77,8 +77,8 @@ class BarCodeComponent extends Component
         $p_checkDigit = isset($options['p_checkDigit']) ? isset($options['p_checkDigit']) : '';
         $p_fileName = isset($options['p_fileName']) ? $options['p_fileName'] : 'code39';
         $p_inverted = $p_invert == 'N';
-        $p_2File = $p_toFile == "N";
         $p_textLabel = $p_label == "N";
+        $p_2File = $p_toFile == "N" ? false : true;
         $p_ck = $p_checkDigit == "N" ? false : true;
 
 
